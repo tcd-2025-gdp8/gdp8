794da7bbd76ee1741110cb5db497660497b6be25
Ivan puig <ivan@bpuig.com>
2025-02-21 13:48:49 +0000

    Create user routes

---
+++ backend/internal/routes/users_routes.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"net/http"
+
+	"firebase.google.com/go/v4/auth"
+
+	"gdp8-backend/internal/handlers"
+	"gdp8-backend/internal/middleware"
+	"gdp8-backend/internal/persistence"
+	"gdp8-backend/internal/repositories"
+	"gdp8-backend/internal/services"
+)
+
+func RegisterModuleUsers(firebaseAuth *auth.Client) {
+	txManager := persistence.MockTransactionManager{}
+	userRepo := repositories.NewMockModuleRepository()
+	userService := services.NewUserService(&txManager, userRepo)
+	handler := handlers.NewUserHandler(userService)
+
+	http.HandleFunc("GET /api/user/{id}", middleware.WithFirebaseAuth(firebaseAuth, handler.GetUser))
+	http.HandleFunc("POST /api/user/modules/{id}", middleware.WithFirebaseAuth(firebaseAuth, handler.SetModules))
+	http.HandleFunc("POST /api/user", middleware.WithFirebaseAuth(firebaseAuth, handler.CreateUser))
+}
