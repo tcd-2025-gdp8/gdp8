42a5c9965b017f6812ffaebc908378647b7811f7
Michal Bronicki <bronickm@tcd.ie>
2025-02-25 18:36:59 +0000

    Added API functions for modules, study groups, and users

---
+++ frontend-web/src/api/users.tsx
@@ -0,0 +1,31 @@
+import { Module } from "./modules";
+import { fetchApi, fetchApiToJson } from "../utils/apiFetch";
+
+export interface UserRegisterDetails {
+    id: string;
+    name: string;
+}
+
+export interface UserModulesSelection {
+    selectedModules: number[];
+}
+
+export async function registerUser(token: string, user: UserRegisterDetails): Promise<void> {
+    const userWithModules = { ...user, modules: [] };
+    await fetchApi("/user", token, {
+        method: "POST",
+        body: JSON.stringify(userWithModules),
+    });
+}
+
+export async function getUserModules(token: string, userId: string): Promise<Module[]> {
+    const modules = await fetchApiToJson<Module[]>(`/user/${userId}/modules`, token);
+    return modules;
+}
+
+export async function updateUserModules(token: string, userId: string, modules: UserModulesSelection): Promise<void> {
+    await fetchApi(`/user/${userId}/modules`, token, {
+        method: "POST",
+        body: JSON.stringify(modules),
+    });
+}
