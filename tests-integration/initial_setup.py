03856796d91103e741f501fe0f542be077f5bf69
Michal Bronicki <bronickm@tcd.ie>
2025-02-25 15:37:25 +0000

    Refactored integration tests setup and config

--- tests-integration/initial_setup.py
+++ tests-integration/initial_setup.py
@@ -3,15 +3,24 @@ import firebase_admin
 from firebase_admin import auth
 
 
-TEST_USER_EMAIL = "testuser@example.com"
-TEST_USER_PASSWORD = "password123"
-TEST_USER_NAME = "Mary Jane Holland"
+TEST_USER = {
+    "email": "testuser@example.com",
+    "password": "password123",
+    "name": "Mary Jane Holland"
+}
 
-TEST_USER2_EMAIL = "user2@example.com"
-TEST_USER2_PASSWORD = "password123"
-
-TEST_USER3_EMAIL = "user3@example.com"
-TEST_USER3_PASSWORD = "password123"
+ADDITIONAL_TEST_USERS = [
+    {
+        "email": "user2@example.com",
+        "password": "password123",
+        "name": "User Two"
+    },
+    {
+        "email": "user3@example.com",
+        "password": "password123",
+        "name": "User Three"
+    }
+]
 
 
 if __name__ == "__main__":
@@ -21,37 +30,17 @@ if __name__ == "__main__":
 
     firebase_admin.initialize_app(None, { "projectId": "demo-backend" })
 
-    # Create Test User 1
-    try:
-        user1 = auth.create_user(
-            email=TEST_USER_EMAIL,
-            password=TEST_USER_PASSWORD,
-            display_name="Test User"
-        )
-        print("Created Test User 1")
-    except Exception as e:
-        print("Test User 1 already exists or error:", e)
+    auth.create_user(
+        email=TEST_USER["email"],
+        password=TEST_USER["password"],
+        display_name="Test User"
+    )
 
-    # Create Test User 2
-    try:
-        user2 = auth.create_user(
-            email=TEST_USER2_EMAIL,
-            password=TEST_USER2_PASSWORD,
-            display_name="Test User 2"
+    for user in ADDITIONAL_TEST_USERS:
+        auth.create_user(
+            email=user["email"],
+            password=user["password"],
+            display_name=user["name"]
         )
-        print("Created Test User 2")
-    except Exception as e:
-        print("Test User 2 already exists or error:", e)
-
-    # Create Test User 3
-    try:
-        user3 = auth.create_user(
-            email=TEST_USER3_EMAIL,
-            password=TEST_USER3_PASSWORD,
-            display_name="Test User 3"
-        )
-        print("Created Test User 3")
-    except Exception as e:
-        print("Test User 3 already exists or error:", e)
 
     print("Initial setup completed.", flush=True)
