33b21f1a5e9b90daa05472061d09ce733e3003e3
Michal Bronicki <bronickm@tcd.ie>
2025-02-26 01:43:05 +0000

    Added schemas for notification tables and added notification deletion trigger

--- backend/migrations/create_and_seed.sql
+++ backend/migrations/create_and_seed.sql
@@ -16,17 +16,22 @@ CREATE TABLE IF NOT EXISTS study_groups (
     type ENUM('public', 'closed', 'invite-only') NOT NULL,
     module_id INT NOT NULL,
     max_members INT NOT NULL,
+
     FOREIGN KEY (module_id) REFERENCES modules(id) ON DELETE RESTRICT,
-    CHECK (max_members > 0),
-    INDEX idx_study_groups_module_id (module_id)
+
+    INDEX idx_study_groups_module_id (module_id),
+
+    CHECK (max_members > 0)
 );
 
 CREATE TABLE IF NOT EXISTS user_modules (
     user_id VARCHAR(255) NOT NULL,
     module_id INT NOT NULL,
+
     PRIMARY KEY (user_id, module_id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (module_id) REFERENCES modules(id) ON DELETE CASCADE,
+
     INDEX idx_user_modules_user_id (user_id),
     INDEX idx_user_modules_module_id (module_id)
 );
@@ -35,9 +40,11 @@ CREATE TABLE IF NOT EXISTS user_study_groups (
     user_id VARCHAR(255) NOT NULL,
     study_group_id INT NOT NULL,
     type ENUM('admin', 'member', 'invitee', 'requester') NOT NULL,
+
     PRIMARY KEY (user_id, study_group_id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE RESTRICT,
     FOREIGN KEY (study_group_id) REFERENCES study_groups(id) ON DELETE CASCADE,
+
     INDEX idx_user_study_groups_user_id (user_id),
     INDEX idx_user_study_groups_study_group_id (study_group_id)
 );
@@ -52,3 +59,70 @@ VALUES
   (6, 'CSU44099', 'Final Year Project'),
   (7, 'CSU44098', 'Group Design Project'),
   (8, 'CSU44081', 'Entrepreneurship & High Tech Venture Creation');
+
+CREATE TABLE IF NOT EXISTS notifications (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    type ENUM(
+        'study-group-joined',
+        'study-group-requested-to-join',
+        'study-group-left',
+        'study-group-accepted-invite',
+        'study-group-rejected-invite',
+        'study-group-invited',
+        'study-group-accepted-join-request',
+        'study-group-rejected-join-request',
+        'study-group-removed-member',
+        'study-group-chat-message'
+        ) NOT NULL,
+    triggering_user_id VARCHAR(255) NOT NULL,
+    target_user_id VARCHAR(255) NULL,
+    study_group_id INT NULL NOT NULL,
+    message_id INT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (triggering_user_id) REFERENCES users(id) ON DELETE RESTRICT,
+    FOREIGN KEY (target_user_id) REFERENCES users(id) ON DELETE RESTRICT,
+    FOREIGN KEY (study_group_id) REFERENCES study_groups(id) ON DELETE CASCADE,
+
+    CHECK (
+        CASE
+            WHEN type = 'study-group-joined'                THEN (target_user_id IS NULL AND message_id IS NULL)
+            WHEN type = 'study-group-requested-to-join'     THEN (target_user_id IS NULL AND message_id IS NULL)
+            WHEN type = 'study-group-left'                  THEN (target_user_id IS NULL AND message_id IS NULL)
+            WHEN type = 'study-group-accepted-invite'       THEN (target_user_id IS NULL AND message_id IS NULL)
+            WHEN type = 'study-group-rejected-invite'       THEN (target_user_id IS NULL AND message_id IS NULL)
+
+            WHEN type = 'study-group-chat-message'          THEN (target_user_id IS NULL AND message_id IS NOT NULL)
+
+            WHEN type = 'study-group-invited'               THEN (target_user_id IS NOT NULL AND message_id IS NULL)
+            WHEN type = 'study-group-accepted-join-request' THEN (target_user_id IS NOT NULL AND message_id IS NULL)
+            WHEN type = 'study-group-rejected-join-request' THEN (target_user_id IS NOT NULL AND message_id IS NULL)
+            WHEN type = 'study-group-removed-member'        THEN (target_user_id IS NOT NULL AND message_id IS NULL)
+
+            ELSE FALSE
+        END = TRUE
+    )
+);
+
+CREATE TABLE IF NOT EXISTS user_notifications (
+    user_id VARCHAR(255) NOT NULL,
+    notification_id INT NOT NULL,
+
+    PRIMARY KEY (user_id, notification_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (notification_id) REFERENCES notifications(id) ON DELETE CASCADE,
+
+    INDEX idx_user_notifications_user_id (user_id),
+    INDEX idx_user_notifications_notification_id (notification_id)
+);
+
+CREATE TRIGGER IF NOT EXISTS delete_notification_when_no_corresponding_users
+    AFTER DELETE ON user_notifications
+    FOR EACH ROW
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM user_notifications WHERE notification_id = OLD.notification_id
+    ) THEN
+        DELETE FROM notifications WHERE id = OLD.notification_id;
+    END IF;
+END;
