82565b0673d2706c7efea58a5b0c848958c33318
Nicoleta Cociug <cociugn@tcd.ie>
2025-03-15 13:11:20 +0000

    Changed the node modules issues

--- frontend-web/src/utils/apiFile.ts
+++ frontend-web/src/utils/apiFile.ts
@@ -2,40 +2,40 @@ import JSZip from "jszip"
 import { makeRequest, fetchApiToJson } from "./apiFetch";
 
 export class BackendFile {
-    name: string;
-    data: Blob;
+  name: string;
+  data: Blob;
 
-    constructor(name: string, data: Blob) {
-        this.name = name;
-        this.data = data;
-    }
+  constructor(name: string, data: Blob) {
+    this.name = name;
+    this.data = data;
+  }
 
-    download() {
-        const url = URL.createObjectURL(this.data);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = this.name;
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        URL.revokeObjectURL(url);
-    }
+  download() {
+    const url = URL.createObjectURL(this.data);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = this.name;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  }
 }
 
 export async function apiFetchFiles(chatID: string, token: string | null): Promise<BackendFile[]> {
-    const response = await makeRequest(`/files/${chatID}`, token);
-    const blob = await response.blob();
-    const zip = await JSZip.loadAsync(blob);
-    const extractedFiles: BackendFile[] = [];
+  const response = await makeRequest(`/files/${chatID}`, token);
+  const blob = await response.blob();
+  const zip = await JSZip.loadAsync(blob);
+  const extractedFiles: BackendFile[] = [];
 
-    for (const filename of Object.keys(zip.files)) {
-        const fileData = zip.files[filename];
-        if (!fileData.dir) {
-            const fileBlob = await fileData.async("blob");
-            extractedFiles.push(new BackendFile(filename, new Blob([fileBlob])));
-        }
+  for (const filename of Object.keys(zip.files)) {
+    const fileData = zip.files[filename];
+    if (!fileData.dir) {
+      const fileBlob = await fileData.async("blob");
+      extractedFiles.push(new BackendFile(filename, new Blob([fileBlob])));
     }
-    return extractedFiles;
+  }
+  return extractedFiles;
 }
 
 export async function apiUploadFiles(
