1a7cef0faf36854d3234d4c47cdb0846b5f79635
Ivan puig <ivan@bpuig.com>
2025-03-28 18:11:46 +0000

    Get results from the LLM

--- backend/internal/chatbot/client.go
+++ backend/internal/chatbot/client.go
@@ -8,14 +8,22 @@ import (
 	"google.golang.org/genai"
 )
 
+// rule of thumb for gemini is 4 characters per token
+// https://ai.google.dev/gemini-api/docs/tokens?lang=go
+// decided to made gemini-1.5-flash our default since it has more capabilities
+// https://ai.google.dev/gemini-api/docs/models#gemini-1.5-flash
 type GeminiClient struct {
-    model string
     Client *genai.Client
+    config *genai.GenerateContentConfig
+    context context.Context
 }
 
-func NewClient(model string) (GeminiClient, error) {
+func NewClient() (GeminiClient, error) {
     ctx := context.Background()
     apiKey := os.Getenv("GEMINI_API_KEY")
+
+    var config *genai.GenerateContentConfig = &genai.GenerateContentConfig{Temperature: genai.Ptr[float32](0.5)}
+
     if apiKey == "" {
         return GeminiClient{}, errors.New("API_KEY not set")
     }
@@ -30,7 +38,27 @@ func NewClient(model string) (GeminiClient, error) {
     }
 
     return GeminiClient{
-        model: model,
         Client: client,
+        config: config,
+        context: ctx,
     }, nil
+}
+
+func (gc *GeminiClient) SetTemperature(temp float32) {
+    gc.config.Temperature = genai.Ptr[float32](temp)
+
+}
+
+func (gc *GeminiClient) Prompt(input string) (string, error) {
+	result, err := gc.Client.Models.GenerateContent(
+        gc.context,
+        "gemini-1.5-flash",
+        genai.Text(input),
+        gc.config)
+
+        if err != nil {
+            return "", err
+        }
+
+        return result.Text(), err;
 }
