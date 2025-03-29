eb462b7aebf43fa01dfe1e632f393434179a2d5c
Ivan puig <ivan@bpuig.com>
2025-03-29 12:48:21 +0000

    Allow authenticated users to interact wiht bot

--- backend/internal/handlers/chatbot_handler.go
+++ backend/internal/handlers/chatbot_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"gdp8-backend/internal/chatbot"
 	"gdp8-backend/internal/services"
 )
 
@@ -11,6 +12,7 @@ type ChatBotHandler struct {
 	fileService       services.FileService
 	chatService       services.ChatService
 	chatbotService    services.ChatBotService
+	client            chatbot.GeminiClient
 }
 
 func NewChatBotHandler(
@@ -18,13 +20,43 @@ func NewChatBotHandler(
 	fileService services.FileService,
 	chatService services.ChatService,
 	chatbotService services.ChatBotService,
+	client chatbot.GeminiClient,
 ) *ChatBotHandler {
 	return &ChatBotHandler{
 		studyGroupService: studyGroupService,
 		fileService:       fileService,
 		chatService:       chatService,
 		chatbotService:    chatbotService,
+		client:            client,
 	}
 }
 
-func (h ChatBotHandler) Test(_ http.ResponseWriter, _ *http.Request) {}
+func (h ChatBotHandler) Prompt(w http.ResponseWriter, r *http.Request) {
+	userMsg := r.FormValue("text")
+	chatID := r.FormValue("chatID")
+	_ = chatID
+	// needMemory := r.FormValue("memory")
+
+	var resp string
+	var err error
+
+	// if needMemory == "true" {
+	// TODO (SCRUM 153):
+	// - query for all the context_data in the group
+
+	// TODO: query db and add the memory in the prompt
+	resp, err = h.client.Prompt(r.Context(), userMsg)
+	// } else {
+	// resp, err = h.client.Prompt(r.Context(), userMsg)
+	// }
+
+	if err != nil {
+		http.Error(w, "Couldn't generate the response", http.StatusInternalServerError)
+	}
+
+	sendJSONResponse(w, map[string]string{"message": resp})
+}
+
+// func (h ChatBotHandler) isUserAllowed(_ *http.Request) bool {
+// 	return true
+// }
--- backend/internal/routes/chatbot_routes.go
+++ backend/internal/routes/chatbot_routes.go
@@ -1,10 +1,12 @@
 package routes
 
 import (
+	"log"
 	"net/http"
 
 	"firebase.google.com/go/v4/auth"
 
+	"gdp8-backend/internal/chatbot"
 	"gdp8-backend/internal/handlers"
 	"gdp8-backend/internal/middleware"
 	"gdp8-backend/internal/services"
@@ -16,11 +18,19 @@ func RegisterChatBotRoutes(
 	fileService services.FileService,
 	chatService services.ChatService,
 	chatbotService services.ChatBotService) {
+
+	client, err := chatbot.NewClient()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
 	handler := handlers.NewChatBotHandler(
 		studyGroupService,
 		fileService,
 		chatService,
-		chatbotService)
+		chatbotService,
+		client)
 
-	http.HandleFunc("POST /api/chatbot/{chatID}", middleware.WithWebSocketAuth(firebaseAuth, handler.Test))
+	http.HandleFunc("POST /api/chatbot", middleware.WithFirebaseAuth(firebaseAuth, handler.Prompt))
 }
