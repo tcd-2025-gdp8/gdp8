649cb741081b80ae46aa3d94204e201f50e05faf
Dezu17 <dariuscpop@gmail.com>
2025-04-01 16:16:44 +0100

    Fixed lint errors in backend

--- backend/internal/models/study_session_availability_request.go
+++ backend/internal/models/study_session_availability_request.go
@@ -5,25 +5,25 @@ import "time"
 type StudySessionAvailabilityRequestID int64
 
 type StudySessionAvailabilityRequest struct {
-	ID           StudySessionAvailabilityRequestID `json:"id" db:"id"`
-	StudyGroupID StudyGroupID                      `json:"study_group_id" db:"study_group_id"`
-	CreatorID    UserID                            `json:"creator_id" db:"creator_id"`
+	ID           StudySessionAvailabilityRequestID
+	StudyGroupID StudyGroupID
+	CreatorID UserID
 	StudySessionAvailabilityRequestDetails
-	Entries []StudySessionAvailabilityEntry `json:"entries"`
+	Entries []StudySessionAvailabilityEntry
 }
 
 type StudySessionAvailabilityRequestDetails struct {
-	Title                   string    `json:"title" db:"title"`
-	AvailabilityPeriodStart time.Time `json:"availability_period_start" db:"availability_period_start"`
-	AvailabilityPeriodEnd   time.Time `json:"availability_period_end" db:"availability_period_end"`
+	Title                   string
+	AvailabilityPeriodStart time.Time
+	AvailabilityPeriodEnd   time.Time
 }
 
 type StudySessionAvailabilityEntry struct {
-	UserID            UserID            `json:"user_id" db:"user_id"`
-	AvailabilityEntry AvailabilityEntry `json:"availability_entry"`
+	UserID UserID
+	AvailabilityEntry
 }
 
 type AvailabilityEntry struct {
-	AvailabilityEntryStart time.Time `json:"availability_entry_start" db:"availability_entry_start"`
-	AvailabilityEntryEnd   time.Time `json:"availability_entry_end" db:"availability_entry_end"`
+	AvailabilityEntryStart time.Time
+	AvailabilityEntryEnd   time.Time
 }
