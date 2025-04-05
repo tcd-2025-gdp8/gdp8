75d0a2e3a3f3913e2b441572c78ea119d195854c
Dezu17 <dariuscpop@gmail.com>
2025-04-05 17:16:35 +0100

    Implemented function to retrieve study group statistics data from the backend

--- frontend-web/src/api/modules.tsx
+++ frontend-web/src/api/modules.tsx
@@ -11,6 +11,18 @@ export interface ModuleCreationDetails {
     name: string;
 }
 
+export interface StudyGroupStatsDTO {
+    id: number;
+    name: string;
+    members: number;
+    totalHours: number;
+    weeklyHours: number[];
+}
+
+export interface StudyGroupStatsMap {
+    [moduleCode: string]: StudyGroupStatsDTO[] | null;
+}
+
 export async function fetchAllModules(token: string | null): Promise<Module[]> {
     const modules = await fetchApiToJson<Module[]>("/modules", token);
     return modules;
@@ -21,4 +33,9 @@ export async function createModule(token: string | null, module: ModuleCreationD
         method: "POST",
         body: JSON.stringify(module),
     });
+}
+
+export async function fetchStudyGroupStats(token: string | null): Promise<StudyGroupStatsMap> {
+    const stats = await fetchApiToJson<StudyGroupStatsMap>("/api/study-group-stats", token);
+    return stats;
 }
